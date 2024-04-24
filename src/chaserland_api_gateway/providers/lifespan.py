c2fb055de85d43bb303b61258099908363ad9bfd
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-24 18:14:37 -0400

    ✨ feat: Utilize lifespan state to manage context such as redis connection pool

--- src/chaserland_api_gateway/providers/lifespan.py
+++ src/chaserland_api_gateway/providers/lifespan.py
@@ -1,26 +1,36 @@
+from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.logger import logger as fastapi_logger
 
-from ..redis.redis import get_redis_session_context, pool
+from ..core.state import LifespanState
+from ..redis.redis import create_redis_pool, get_redis_session_context
 
 
-async def on_startup(app: FastAPI):
+async def on_startup(app: FastAPI) -> LifespanState:
+    fastapi_logger.info(f"Initializing Redis connection pool...")
+    redis_pool = create_redis_pool()
+
     fastapi_logger.info(f"Testing Redis connection...")
-    async with get_redis_session_context() as redis:
+    async with get_redis_session_context(pool=redis_pool) as redis:
         await redis.ping()
+
+    state = LifespanState(redis_pool=redis_pool)
+
     fastapi_logger.info(f"{app.title} server ready to accept connections.")
+    return state
 
 
-async def on_shutdown(app: FastAPI):
+async def on_shutdown(app: FastAPI, state: LifespanState):
     fastapi_logger.info(f"Closing Redis connection...")
-    await pool.aclose()
+    await state["redis_pool"].aclose()
+
     fastapi_logger.info(f"{app.title} server stopped.")
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    await on_startup(app)
-    yield
-    await on_shutdown(app)
+async def lifespan(app: FastAPI) -> AsyncIterator[LifespanState]:
+    state = await on_startup(app)
+    yield state
+    await on_shutdown(app, state)
