82bb27a78b7c50f051a2633be18cce2d89acd7dd
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-30 03:42:57 -0400

    ✨ feat: Update SubAppProvider to support dynamic addition of sub-apps

--- src/chaserland_api_gateway/providers/route.py
+++ src/chaserland_api_gateway/providers/route.py
@@ -11,24 +11,30 @@ def traverse_routes(app: FastAPI):
         route, level = queue.pop()
         if isinstance(route, Mount):
             fastapi_logger.debug(
-                f"{'   ' * level}{route.__class__.__name__}: {route.path} - {route.name}"
+                f"{'   ' * level}{type(route).__name__}: {route.path} - {route.name}"
             )
             queue.extend([(r, level + 1) for r in route.app.routes])
         else:
             route: Route
             fastapi_logger.debug(
-                f"{'   ' * level}{route.__class__.__name__}: {route.path} - {route.name} - {route.methods}"
+                f"{'   ' * level}{type(route).__name__}: {route.path} - {route.name} - {route.methods}"
             )
 
 
-class RouteProvider(AbstractFastAPIComponentProvider):
-    @staticmethod
-    def register(app: FastAPI):
+class SubAppProvider(AbstractFastAPIComponentProvider):
+    def __init__(self, mounts: dict[str, FastAPI] = {}):
+        self.mounts = mounts
+
+    def add_app(self, path: str, app: FastAPI):
+        self.mounts[path] = app
+
+    def register(self, app: FastAPI):
         """Register FastAPI routes.
 
         Args:
             app (FastAPI): FastAPI app instance.
         """
-
+        for path, sub_app in self.mounts.items():
+            app.mount(path, sub_app, name=sub_app.title)
         if app.debug:
             traverse_routes(app)
